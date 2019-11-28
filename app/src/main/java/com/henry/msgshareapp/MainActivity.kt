b52412a9389c3aa15cffa99b5ba9a032d3a54e7c
polar <km3684629@gmail.com>
2019-11-27 20:08:44 -0500

    Section Two Video Five

--- app/src/main/java/com/henry/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/henry/msgshareapp/MainActivity.kt
@@ -25,5 +25,16 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("user_message", message)
             startActivity(intent)
         }
+
+        btnShare.setOnClickListener {
+            val message:String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to : "))
+
+        }
     }
 }
