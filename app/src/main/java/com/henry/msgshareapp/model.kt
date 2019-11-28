e11f174a9d10a45f60e0695dc77f65fb484a5470
polar <km3684629@gmail.com>
2019-11-28 12:54:00 -0500

    Section Three Video two

---
+++ app/src/main/java/com/henry/msgshareapp/model.kt
@@ -0,0 +1,30 @@
+package com.henry.msgshareapp
+
+data class Hobby(var title:String)
+
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Talking")
+    )
+}
